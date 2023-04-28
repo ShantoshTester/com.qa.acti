f9f31757092db8939b681db2f9f217b0de563fc0
shantosh <shantosh25@hotmail.com>
2023-04-28 08:19:02 +0530

    added author details to enterpage

--- src/main/java/com/acti/pages/EnterPage.java
+++ src/main/java/com/acti/pages/EnterPage.java
@@ -2,6 +2,15 @@ package com.acti.pages;
 
 import com.acti.base.DriverScript;
 
+/*
+ * Name: EnterPage
+ * Description: Manages the locators and methods of enter page
+ * Developed By: Satya
+ * Reviewed By: Shantosh
+ * Date Review : 04-27-2023
+ */
+
+
 public class EnterPage extends DriverScript {
 
 }
